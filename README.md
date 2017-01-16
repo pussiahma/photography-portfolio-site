f4df328fc81ea79a82bd260e78b681e986a0cc92
pussiahma <ella.ruokokoski@gmail.com>
2017-01-16 19:40:47 +0200

    first day's work

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # photography-portfolio-site
+
+I am not a professional photographer but photography is a serious hobby for me  even though I know I have a lot to learn. I have always loved the minimalistic style of many photographers' websites and I decided to make my own inspired by those styles.
+
+
+//This site is under construction
---
+++ js/photography.js
@@ -0,0 +1,38 @@
+$(".nav .menuitem a").click(function() {
+  $(".nav .menuitem a").removeClass("selected");
+   $(this).addClass("selected");
+    return false;
+});
+
+$(".nav .headline a").click(function() {
+	$(".nav .menuitem a").removeClass("selected");
+console.log("header clicked");
+});
+
+
+	$($("nav .menuitem a")).on('click', function(e) {
+   e.preventDefault();
+//varastoi klikatun hashin
+   let hash = this.hash;
+   //luodaananimaatio, sekunnin kesto
+   $('html, body').animate({
+    //kuinka monta pikseliä klikatun hashin yläpuolella on ikkunassa
+    //tämä on se matka jonka animaatio kulkee klikattaessa, joko ylös tai alas
+       scrollTop: $(hash).offset().top-100
+     },1000, function(){
+        //lisää urliin klikatun hashin
+       window.location.hash = hash;
+     });
+
+}); 
+
+
+
+//mansonrykoodi
+
+
+ $('.container-fluid').masonry({
+        itemSelector: '.item',
+        columnWidth: 420
+
+});
