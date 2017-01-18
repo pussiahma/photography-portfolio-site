57b2df9a9f84491941abc44343575a33b3e81ad5
pussiahma <ella.ruokokoski@gmail.com>
2017-01-18 14:42:18 +0200

    switched to masonry because got tired of watching gaps in boostrap grid. Masonry didn't work well with lazy load so got rid of that and added my own animation to pictures with css and javascript

--- js/photography.js
+++ js/photography.js
@@ -24,10 +24,13 @@ let aboutOffset = $("footer #about").offset().top-120;
 let port = $("nav #port");
 let ab = $("nav #ab");
 let scrollTop = $(this).scrollTop();
+let item = $(".grid .item");
+
 
 if(scrollTop > portfolioOffset&& scrollTop < aboutOffset) {
 	port.addClass("selected");
 	ab.removeClass("selected");
+
 }else if(scrollTop > aboutOffset) {
 	ab.addClass("selected");
 	port.removeClass("selected");
@@ -36,10 +39,21 @@ if(scrollTop > portfolioOffset&& scrollTop < aboutOffset) {
 	ab.removeClass("selected");
 
 }
+item.each(function() {
+
+let itemOffset = $(this).offset().top-500;
+
+		if(scrollTop > itemOffset) { 
+		$(this).css("opacity", "1").css("transform", "translate3d(0,0,0)").css("transition", "opacity 1.5s, transform 1s");
+		console.log("worked?");
+	}
+		});
+
 
 
 
 });
+
 
 
 
@@ -55,11 +69,11 @@ $('.lazyload').lazyload({load: load});
 
 
 //masonrykoodi
-/*
+
 
  $('.container-fluid').masonry({
-        itemSelector: '.item',
-        columnWidth: 607.55
+        columnWidth: '.grid-sizer',
+itemSelector: '.item',
+isFitWidth: true
 
 });
-*/
