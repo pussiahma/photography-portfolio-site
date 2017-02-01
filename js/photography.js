70cf5a2dd7960068751793f4708dbf6d5976a1b9
pussiahma <ella.ruokokoski@gmail.com>
2017-02-01 18:34:22 +0200

    header-height increase

--- js/photography.js
+++ js/photography.js
@@ -10,15 +10,11 @@ isFitWidth: true
 
 	$($(".menuitem a")).on('click', function(e) {
    e.preventDefault();
-//varastoi klikatun hashin
    let hash = this.hash;
-   //luodaananimaatio, sekunnin kesto
    $('html, body').animate({
-    //kuinka monta pikseliä klikatun hashin yläpuolella on ikkunassa
-    //tämä on se matka jonka animaatio kulkee klikattaessa, joko ylös tai alas
        scrollTop: $(hash).offset().top
      },1000, function(){
-        //lisää urliin klikatun hashin
+
        window.location.hash = hash;
      });
 
@@ -28,7 +24,7 @@ isFitWidth: true
 $(window).scroll(function() {
 
 let portfolioOffset = $("#portfolio").offset().top-120;
-let aboutOffset = $("footer #about").offset().top-120;
+let aboutOffset = $("footer #about").offset().top-10;
 let port = $("nav #port");
 let ab = $("nav #ab");
 let scrollTop = $(this).scrollTop();
